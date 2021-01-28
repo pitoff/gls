41e143b84df164ab3a83200735621cdd2d83feec
pitoff <61664958+pitoff@users.noreply.github.com>
2021-01-28 12:05:03 +0100

    Update account.php

--- app/view/users/account.php
+++ app/view/users/account.php
@@ -1,3 +1,8 @@
+<style type="text/css">
+  .detailstyle{
+  font-weight: bold;
+}
+</style>
 <?php require APPROOT .'/view/include/header'. '.php';?>      
       <section id="main-content">
           <section class="wrapper">            
@@ -9,13 +14,15 @@
   						<li><i class="fa fa-bank"></i>Account Summary</li>						  	
   					</ol>
   				</div>
+        
 
-			  	   <div class="col-lg-12">
+          
+			  	   <div class="col-lg-8 col-lg-offset-2">
                       <div class="table-responsive">
                           <header class="panel-heading">
                               Checking & Savings
                           </header>
-                          <table class="table table-hover table-condensed">
+                          <!-- <table class="table table-hover table-condensed">
                               <thead>
                               <tr>
                               	  <th>Acc number</th>
@@ -23,24 +30,30 @@
                                   <th>availabe balance</th>
                                   <th>last transaction</th>
                               </tr>
-                              </thead>
+                              </thead> -->
                             <?php foreach($data['all'] as $all):?>
                             	<?php if(($data['user']->id) == ($all->user_id)):?>
-                              <tbody>
-                              <tr>
+                              <!-- <tbody> -->
+                              <!-- <tr>
                               	  <td><?php echo $all->acc_num;?></td>
                                   <td>&#36;<?php echo number_format($all->current_bal, 2);?></td>
                                   <td>&#36;<?php echo number_format($all->available_bal, 2);?></td>
                                   <td><?php echo $all->dod;?></td>
                               </tr>
-                              </tbody>
+                              </tbody> -->
+                              <div style="padding-top: 5px; padding-left: 5px;">
+                              <p><span class="detailstyle">Account Number: <?php echo $all->acc_num;?></span></p>
+                              <p><span class="detailstyle">Current Balance: &#36;<?php echo number_format($all->current_bal, 2);?></span></p>
+                              <p><span class="detailstyle">Available Balance: &#36;<?php echo number_format($all->available_bal, 2);?></span></p>
+                              <p><span class="detailstyle">Last transaction: <?php echo $all->dod;?></span></p>
+                              </div>
                           <?php endif;?>
                           	<?php endforeach;?>
                               
-                          </table>
-                          <td>
+                          <!-- </table> -->
+                          <p>
 	                        <!-- <a href="<?php echo URLROOT;?>/users/transfer/<?php echo $all->depositId;?>"> --><button class="btn btn-primary" type="submit" disabled="disabled" style="margin-top: 10px;">Transfer Funds</button>
-  							         </td>
+  							         </p>
                       </div>
                       <div>Dear <?php echo $data['user']->firstname;?> your account has been restricted due to inactivity!</div>
                   </div>
